72f1519f4bff799bae2a6039d5386d472dab0d66
Victor Prouff <v.prouff-ext@effy.fr>
2021-03-11 13:29:34 +0100

    Copy AmountFor in PerformanceCalculator

--- src/Refactoring-Example/Models/Performance.cs
+++ src/Refactoring-Example/Models/Performance.cs
@@ -2,13 +2,16 @@ namespace Refactoring_Example.Models
 {
     public class Performance
     {
+        private readonly PerformanceCalculator Calculator;
         public Performance(Play play, int audience)
         {
             Play = play;
             Audience = audience;
+            Calculator = new PerformanceCalculator(this);
         }
 
         public Play Play { get; }
         public int Audience { get; }
+        public long Amount => Calculator.Amount;
     }
 }
--- src/Refactoring-Example/PerformanceCalculator.cs
+++ src/Refactoring-Example/PerformanceCalculator.cs
@@ -11,5 +11,36 @@ namespace Refactoring_Example
         }
 
         public Performance Performance { get; }
+
+        public long Amount => AmountFor();
+
+        private long AmountFor()
+        {
+            long result;
+            switch (Performance.Play.Type)
+            {
+                case "tragedy":
+                    result = 40000;
+                    if (Performance.Audience > 30)
+                    {
+                        result += 1000 * (Performance.Audience - 30);
+                    }
+
+                    break;
+                case "comedy":
+                    result = 30000;
+                    if (Performance.Audience > 20)
+                    {
+                        result += 10000 + 500 * (Performance.Audience - 20);
+                    }
+
+                    result += 300 * Performance.Audience;
+                    break;
+                default:
+                    throw new Exception($"Unkwnon type : {Performance.Play.Type}");
+            }
+
+            return result / 100;
+        }
     }
 }
